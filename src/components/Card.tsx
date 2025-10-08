2bc81ecb80583c3690b44b755ff9c2e0160d895e
presenty1897 <presenty1897@gmail.com>
2025-10-08 18:40:17 +0900

    feat: Implement 3D card component with Framer Motion

---
+++ src/components/Card.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
+import './Card.css';
+
+const Card = () => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const rotateX = useTransform(y, [-100, 100], [30, -30]);
+  const rotateY = useTransform(x, [-100, 100], [-30, 30]);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    const xPct = mouseX / width - 0.5;
+    const yPct = mouseY / height - 0.5;
+
+    x.set(xPct * 200);
+    y.set(yPct * 200);
+  };
+
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
+
+  return (
+    <motion.div
+      className="card-container"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        rotateX,
+        rotateY,
+        transformStyle: 'preserve-3d',
+      }}
+    >
+      <div className="card">
+        <div className="card-content">
+          {/* Image or Video will go here */}
+          <p>Hover over me</p>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Card;
