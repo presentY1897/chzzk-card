e2e113f14eb36c4ead053623a7cb3dc08af28150
presenty1897 <presenty1897@gmail.com>
2025-10-21 18:38:57 +0900

    feat: clip list 조회 api hook 추가

--- src/hooks/useChzzkFetch.ts
+++ src/hooks/useChzzkFetch.ts
@@ -1,4 +1,4 @@
-import type { ChzzkClipInfo } from "@/types";
+import type { ChzzkClipInfo, ChzzkClipPreviewInfo } from "@/types";
 import { useEffect, useState } from "react";
 
 
@@ -23,5 +23,29 @@ export const useFetchChzzkClipInfo = (clipId?: string) => {
 	}, [clipId]);
 
 	return clipInfo;
+
+}
+
+const CHZZK_CLIP_RECOMMENDED_LIST_API_ENDPOINT = "/api/service/v1/home/recommended/clips";
+
+export const useFetchChzzkClipRecommendedList = () => {
+	const [clipList, setClipList] = useState<ChzzkClipPreviewInfo[]>([]);
+
+	useEffect(() => {
+		const fetchClipList = async () => {
+			try {
+				const options = '';// '?filterType=WITHIN_1_DAY&orderType=RECOMMEND';
+				const apiURL = `${CHZZK_CLIP_RECOMMENDED_LIST_API_ENDPOINT}${options}`;
+				const response = await fetch(apiURL);
+				const data = await response.json();
+				setClipList(data?.content?.data);
+			} catch (error) {
+				console.error('Error fetching clip list:', error);
+			}
+		}
+		fetchClipList();
+	}, []);
+
+	return clipList;
 
 }
