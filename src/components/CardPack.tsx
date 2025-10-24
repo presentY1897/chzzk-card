e3f2e7b273ea84e9670db8076c37f9b21fca3101
presenty1897 <presenty1897@gmail.com>
2025-10-24 17:08:16 +0900

    feat: card pack 기본 구현

--- src/components/CardPack.tsx
+++ src/components/CardPack.tsx
@@ -1,107 +1,150 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
-import Card from "./card/Card";
-import type { CardData } from "@/types";
+import type { ChzzkClipPreviewInfo } from "@/types";
 import "./CardPack.css";
+import CardStack from "./CardStack";
+import Cube from "./Cube";
+import Card from "./card/Card";
+import { convertChzzkPreviewClipInfoToCardData } from "@/tools/dataTool";
 
-interface CardPackProps {
-  card: CardData;
-}
+const CardPack = ({
+  cardList,
+  packName,
+}: {
+  cardList: ChzzkClipPreviewInfo[];
+  packName: string;
+}) => {
+  const [isBackVisible, setIsBackVisible] = useState(true);
 
-const PACK_WIDTH = 260;
-
-const wrapperVariants = {
-  open: {
-    x: [-PACK_WIDTH / 2, 0],
-    scale: [0.9, 1],
-    opacity: [0.8, 1],
-    transition: { duration: 0.4 },
-  },
-  closed: {
-    x: 0,
-    scale: 1,
-    opacity: 1,
-  },
-};
-
-const cardVariants = {
-  hidden: {
-    scale: 0.5,
-    rotateY: 90,
-    opacity: 0,
-  },
-  visible: {
-    scale: 1,
-    rotateY: 0,
-    opacity: 1,
-    transition: { delay: 0.4, duration: 0.5, ease: "easeOut" },
-  },
-};
-
-const flashVariants = {
-  hidden: { scale: 0, opacity: 0 },
-  visible: {
-    scale: 1,
-    opacity: [0, 1, 0],
-    transition: { duration: 0.4, delay: 0.2 },
-  },
-};
-
-const CardPack = ({ card }: CardPackProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleDragEnd = (
-    event: MouseEvent | TouchEvent | PointerEvent,
-    info: any
-  ) => {
-    if (Math.abs(info.offset.x) > PACK_WIDTH / 3) {
-      setIsOpen(true);
-    }
+  const handleBackDoubleClick = () => {
+    setIsBackVisible(false);
   };
 
   return (
     <div className="card-pack-container">
-      <motion.div
-        className="card-reveal-wrapper"
-        variants={cardVariants}
-        initial="hidden"
-        animate={isOpen ? "visible" : "hidden"}
-      >
-        <Card {...card} isAnimating={!isOpen} />
-      </motion.div>
-
-      <motion.div
-        className="flash-effect"
-        variants={flashVariants}
-        initial="hidden"
-        animate={isOpen ? "visible" : "hidden"}
+      <Cube
+        frontContent={
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "rgba(25,25,25, 1)",
+                width: "98%",
+                height: "10%",
+              }}
+            ></div>
+            <div
+              style={{
+                backgroundColor: "rgba(30, 30, 30, 1)",
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                gap: "35px",
+                alignItems: "center",
+                justifyContent: "center",
+                position: "relative",
+              }}
+            >
+              <div className="card-pack-strip" />
+              <img
+                style={{ width: "70%", transform: "rotate(-10deg)" }}
+                src={"./images/chzzklogo_kor(Green).png"}
+                draggable={false}
+              />
+              <span style={{ color: "white", fontWeight: "bold" }}>
+                {packName} 팩
+              </span>
+              <div className="card-pack-strip downside" />
+            </div>
+            <div
+              style={{
+                backgroundColor: "rgba(25,25,25, 1)",
+                width: "98%",
+                height: "10%",
+              }}
+            ></div>
+          </div>
+        }
+        backContent={
+          <>
+            <CardStack
+              cardList={cardList
+                ?.map((item: ChzzkClipPreviewInfo, index: number) => (
+                  <Card
+                    key={index}
+                    card={convertChzzkPreviewClipInfoToCardData(item, index)}
+                    initialCardFaceState="front"
+                  />
+                ))
+                .slice(0, 10)}
+            />
+            <div
+              onDoubleClick={handleBackDoubleClick}
+              style={{
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                zIndex: 99999,
+                display: isBackVisible ? "flex" : "none",
+              }}
+            >
+              <div
+                style={{ width: "50%", backgroundColor: "rgba(20,20,20, 1)" }}
+              ></div>
+              <div
+                style={{ width: "50%", backgroundColor: "rgba(30, 30, 30, 1)" }}
+              ></div>
+            </div>
+          </>
+        }
+        leftContent={
+          <div
+            style={{
+              backgroundColor: "rgba(30, 30, 30, 1)",
+              width: "100%",
+              height: "100%",
+            }}
+          ></div>
+        }
+        rightContent={
+          <div
+            style={{
+              backgroundColor: "rgba(30, 30, 30, 1)",
+              width: "100%",
+              height: "100%",
+            }}
+          ></div>
+        }
+        topContent={
+          <div
+            style={{
+              backgroundColor: "rgba(30, 30, 30, 1)",
+              width: "100%",
+              height: "100%",
+            }}
+          ></div>
+        }
+        bottomContent={
+          <div
+            style={{
+              backgroundColor: "rgba(30, 30, 30, 1)",
+              width: "100%",
+              height: "100%",
+            }}
+          ></div>
+        }
+        halfHeight={200}
+        halfWidth={130}
+        halfLength={3}
       />
-
-      {!isOpen && (
-        <motion.div
-          className="pack-wrapper"
-          initial={{ opacity: 1 }}
-          animate={{ opacity: isOpen ? 0 : 1, transition: { delay: 0.2 } }}
-        >
-          <motion.div
-            className="pack-tear-grip"
-            drag="x"
-            dragConstraints={{ left: -(PACK_WIDTH / 2), right: PACK_WIDTH / 2 }}
-            dragElasticity={0.2}
-            onDragEnd={handleDragEnd}
-          />
-          <motion.div
-            className="pack-front left"
-            animate={{ x: isOpen ? -PACK_WIDTH : 0, opacity: isOpen ? 0 : 1 }}
-            transition={{ duration: 0.4 }}
-          />
-          <motion.div
-            className="pack-front right"
-            animate={{ x: isOpen ? PACK_WIDTH : 0, opacity: isOpen ? 0 : 1 }}
-            transition={{ duration: 0.4 }}
-          />
-        </motion.div>
-      )}
     </div>
   );
 };
