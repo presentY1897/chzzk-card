7a023b7adb210a9cc8975db1dd8e928295a9fd34
presenty1897 <presenty1897@gmail.com>
2025-10-26 17:49:38 +0900

    refactor: page 기능을 위해 카드팩/카드 리스트 페이지 구현

---
+++ src/pages/CardPackListPage.tsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import CardPack from "@/components/card-pack/CardPack";
+import { useFetchChzzkClipRecommendedList } from "@/hooks/useChzzkFetch";
+import type { ChzzkClipPreviewInfo } from "@/types";
+import "@/App.css";
+import CardStack from "@/components/CardStack";
+
+function CardPackListPage() {
+  const { data: cardList, loading, error } = useFetchChzzkClipRecommendedList();
+  const [groupedPacks, setGroupedPacks] = useState<
+    Record<string, ChzzkClipPreviewInfo[]>
+  >({});
+
+  useEffect(() => {
+    if (cardList && cardList.length > 0) {
+      const grouped = cardList.reduce((acc, item) => {
+        const category = item.clipCategory || "기타";
+        if (!acc[category]) {
+          acc[category] = [];
+        }
+        acc[category].push(item);
+        return acc;
+      }, {} as Record<string, ChzzkClipPreviewInfo[]>);
+      setGroupedPacks(grouped);
+    }
+  }, [cardList]);
+
+  if (loading) {
+    return <div className="loading-message">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error-message">Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <CardStack
+          cardList={Object.keys(groupedPacks)
+            .filter((category) => groupedPacks[category].length > 0)
+            .map((category) => (
+              <CardPack
+                key={category}
+                cardList={groupedPacks[category]}
+                packName={category}
+              />
+            ))}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CardPackListPage;
