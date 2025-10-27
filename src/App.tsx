ed7e91568591b5d57407d1552b15f40ee077ec9a
presenty1897 <presenty1897@gmail.com>
2025-10-27 16:21:44 +0900

    feat: add card stack page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import CardPackListPage from "./pages/CardPackListPage";
 import CardListPage from "./pages/CardListPage";
+import CardStackPage from "./pages/CardStackPage";
 import "./App.css";
 
 function App() {
@@ -9,11 +10,13 @@ function App() {
       <nav className="navigation-menu">
         <Link to="/card-pack-list">Card Packs</Link>
         <Link to="/card-list">Card List</Link>
+        <Link to="/card-stack">Card Stack</Link>
       </nav>
       <Routes>
         <Route path="/" element={<CardPackListPage />} />
         <Route path="/card-pack-list" element={<CardPackListPage />} />
         <Route path="/card-list" element={<CardListPage />} />
+        <Route path="/card-stack" element={<CardStackPage />} />
       </Routes>
     </div>
   );
