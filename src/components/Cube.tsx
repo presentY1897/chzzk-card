910ee21d242da4426c283b094c6d59064b9e4360
presenty1897 <presenty1897@gmail.com>
2025-10-29 11:36:31 +0900

    refactor: change file name

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { type ReactNode } from "react";
-import { useInteractiveTransform } from "@/hooks/useCubeRotate";
+import { useInteractiveTransform } from "@/hooks/useInteractiveTransform";
 import "./Cube.css";
 
 export interface CubeProps {
